a45db7e9af46b1663b18b6b5dd5352591549bb9a
Matheus da Silva <contagodzillajapan@gmail.com>
2024-08-03 16:14:54 -0300

    test: criar produto para teste

---
+++ src/pages/produtos/exemplos.jsx
@@ -0,0 +1,9 @@
+export const exampleProduct = {
+    id: '1',
+    name: 'Papel Cartão Branco (A4)',
+    price: 'R$ 29,90',
+    description: 'Pacote com 125 Folhas - \n Papel Branco Grosso 180g/m²',
+    image: '/img/papel cartão.webp',
+    status: 'Disponível',
+};
+  
