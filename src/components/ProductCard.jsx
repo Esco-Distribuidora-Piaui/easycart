d477937a3672f0ea5d92dbf3e4737a05a203c245
Matheus da Silva <contagodzillajapan@gmail.com>
2024-08-03 21:11:59 -0300

    feat(comprador): desenvolvendo Card de Produtos

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,9 +1,29 @@
+import React from 'react';
+import Image from 'next/image';
+import styles from '../styles/ProductCard.module.css';
+
 const ProductCard = ({ product }) => (
-    <div>
-      <h2>{product.name}</h2>
-      <p>{product.price}</p>
+  <div className={styles.productCard}>
+    <div className={styles.imageWrapper}>
+      <Image src={product.image} alt={product.name} 
+          width={100}
+          height={100} 
+          layout="responsive"/>
+      <div className={styles.cartIcon}>
+        <Image 
+          src="/icons/cart.svg" 
+          alt="Adicionar ao Carrinho" 
+          width={30} 
+          height={30} 
+        />
+      </div>
     </div>
-  );
-  
-  export default ProductCard;
-  
+    <div className={styles.productInfo}>
+      <h2 className={styles.productName}>{product.name}</h2>
+      <p className={styles.productPrice}>{product.price}</p>
+      <p className={styles.productStatus}>{product.status}</p>
+    </div>
+  </div>
+);
+
+export default ProductCard;
