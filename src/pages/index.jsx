90b47a6030470a5ef531bbfce4fc7fb402b88eb9
RodrigoFontes377 <rodrigofontes377@gmail.com>
2024-08-04 17:41:39 -0300

    teste de instanciação

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,19 +1,21 @@
-import { useEffect, useState } from 'react';
-import ProductCard from '../components/ProductCard';
-import api from '../services/api';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
+import { useEffect, useState } from "react";
+import ProductCard from "../components/ProductCard";
+import api from "../services/api";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import { exampleProduct } from "./produtos/exemplos";
+import ProductCardAdmin from "../components/ProductCardAdmin";
 
 const Home = () => {
-  const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState([exampleProduct]);
 
   useEffect(() => {
     const fetchProducts = async () => {
       try {
-        const data = await api.get('/products');
-        setProducts(data);
+        const response = await api.get("/products");
+        setProducts(response.data);
       } catch (error) {
-        console.error('Failed to fetch products:', error);
+        console.error("Failed to fetch products:", error);
       }
     };
 
@@ -21,12 +23,20 @@ const Home = () => {
   }, []);
 
   return (
-    <div>
+    <div className="flex flex-col min-h-screen">
       <Header />
-      <main>
-        {products.map((product) => (
-          <ProductCard key={product.id} product={product} />
-        ))}
+      <main
+        className="flex-grow"
+        style={{ backgroundColor: "var(--page-bg-color)" }}
+      >
+        <div className="p-4">
+          {products.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+          {products.map((product) => (
+            <ProductCardAdmin key={product.id} product={product} />
+          ))}
+        </div>
       </main>
       <Footer />
     </div>
