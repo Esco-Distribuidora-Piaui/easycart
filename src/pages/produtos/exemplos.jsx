fd54ef3d8392633d13f36051a187a48096abcaa4
Matheus da Silva <contagodzillajapan@gmail.com>
2024-08-04 21:05:50 -0300

    Test: mais produtos

--- src/pages/produtos/exemplos.jsx
+++ src/pages/produtos/exemplos.jsx
@@ -1,9 +1,20 @@
-export const exampleProduct = {
+export const exampleProducts = [
+  {
     id: '1',
     name: 'Papel Cartão Branco (A4)',
     price: 'R$ 29,90',
     description: 'Pacote com 125 Folhas - \n Papel Branco Grosso 180g/m²',
     image: '/img/papel cartão.webp',
     status: 'Disponível',
-};
-  
+    tag: 'Papel'
+  },
+  {
+    id: '2',
+    name: 'Capa para Encadernação Preta (A4)',
+    price: 'R$ 14,50',
+    description: 'Pacote com 10 Unidades - \n Capa de PVC para Encadernação A4',
+    image: '/img/capa_encadernacao_preta.webp',
+    status: 'Disponível',
+    tag: 'Capa para encadernação'
+  },
+];
