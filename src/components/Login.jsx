96b8bee64f759dba0582a71b2380c24214dba81e
RodrigoFontes377 <rodrigofontes377@gmail.com>
2024-08-04 21:21:56 -0300

    començando a tela de login

---
+++ src/components/Login.jsx
@@ -0,0 +1,85 @@
+import React from "react";
+import styles from "../styles/Login.module.css";
+
+export default function Login({ onLogin }) {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onLogin();
+  };
+
+  return (
+    <div className={styles.loginContainer}>
+      <div className={styles.loginFormWrapper}>
+        <img
+          alt="Sua Empresa"
+          src="/logo-esco.png"
+          className="mx-auto h-20 w-auto"
+        />
+        <h2
+          className={`text-2xl font-bold leading-9 tracking-tight ${styles.loginTitle}`}
+        >
+          Entrar na sua conta
+        </h2>
+
+        <form onSubmit={handleSubmit} className="space-y-6">
+          <div>
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium leading-6 text-black"
+            >
+              Endereço de Email
+            </label>
+            <div className="mt-2">
+              <input
+                id="email"
+                name="email"
+                type="email"
+                required
+                autoComplete="email"
+                className={`block w-full rounded-md border-0 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 ${styles.inputText}`}
+              />
+            </div>
+          </div>
+
+          <div>
+            <div className="flex items-center justify-between">
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium leading-6 text-black"
+              >
+                Senha
+              </label>
+              <div className="text-sm">
+                <a
+                  href="#"
+                  className="font-semibold text-indigo-600 hover:text-indigo-500"
+                >
+                  Esqueceu a senha?
+                </a>
+              </div>
+            </div>
+            <div className="mt-2">
+              <input
+                id="password"
+                name="password"
+                type="password"
+                required
+                autoComplete="current-password"
+                className={`block w-full rounded-md border-0 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 ${styles.inputText}`}
+              />
+            </div>
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Entrar
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
