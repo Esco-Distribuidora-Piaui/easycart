08e56215adb30b46219d38b38a3f72947f55237d
Marcos Paulo <marcos.paulo.s.m.filho@gmail.com>
2024-08-09 17:43:56 -0300

    Feat: estrutura pag cliente

--- src/pages/admin/meus-produtos.jsx
+++ src/pages/admin/meus-produtos.jsx
@@ -1,2 +1,11 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+
+export default function Home() {
+    return (
+        <div className="flex flex-col min-h-screen">
+            <Header />
+            <Footer />
+        </div>
+    );
+  }
