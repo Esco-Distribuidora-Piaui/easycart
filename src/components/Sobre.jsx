6e9bab416510d00d64631342265d1c1be581f845
Marcos Paulo <marcos.paulo.s.m.filho@gmail.com>
2024-08-23 17:50:22 -0300

    feat - sobre

---
+++ src/components/Sobre.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Sobre = () => {
+  return (
+    <section id="sobre" className="text-center pt-20 mt-20 mb-20 py-8">
+        <div className="max-w-2xl mx-auto text-center">
+            <h1 className="text-4xl font-bold">EasyCart</h1>
+            <p className="text-lg mt-4 font-openSans">
+                O EasyCart é uma vitrine digital projetada para quem busca uma plataforma eficiente e acessível para expor e vender seus produtos online, simplificando cada etapa do processo de vendas, desde a exposição dos produtos até a montagem do pedido.
+            </p>
+        </div>
+    </section>
+  );
+};
+
+export default Sobre;
