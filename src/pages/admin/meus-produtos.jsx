d93bbacfaaa5c643918576469408a59875798a2f
Matheus da Silva <contagodzillajapan@gmail.com>
2024-08-30 17:25:02 -0300

    Test: tentando fazer deploy

--- src/pages/admin/meus-produtos.jsx
+++ src/pages/admin/meus-produtos.jsx
@@ -1,11 +1,11 @@
-import Header from "../components/Header";
-import Footer from "../components/Footer";
+// import Header from "../components/Header";
+// import Footer from "../components/Footer";
 
-export default function Home() {
-    return (
-        <div className="flex flex-col min-h-screen">
-            <Header />
-            <Footer />
-        </div>
-    );
-  }
+// export default function Home() {
+//     return (
+//         <div className="flex flex-col min-h-screen">
+//             <Header />
+//             <Footer />
+//         </div>
+//     );
+//   }
