88a42a90962db0258ac78c23c7874b6ce7519ca9
RodrigoFontes377 <rodrigofontes377@gmail.com>
2024-08-31 21:32:54 -0300

    modificando o header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,61 +1,39 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 import ToggleThemeButton from "./ToggleThemeButton";
+import styles from "../styles/Header.module.css"; // Importando o CSS corretamente
 
 const Header = () => {
-  const primaryColor = "var(--primary-color)";
-  const headerBgColor = "var(--secondary-color)";
-  const textColor = "var(--primary-color)";
-  const inputBgColor = "bg-white";
-  const indicatorBgColor = "bg-red-600";
-  const badgeBgColor = "bg-green-600";
-  const hoverBgColor = "hover:bg-gray-400";
-  const menuBgColor = "bg-white";
-  const menuHoverBgColor = "hover:bg-gray-200";
-  const textGrayColor = "var(--primary-color)";
-
   return (
     <>
-      <div className="navbar">
-        <div className="flex-1 flex justify-between items-center">
+      <div className={styles.navbar}>
+        <div className={`${styles["flex-container"]} flex-1`}>
           {/* logo */}
-          <div className="flex items-center gap-2">
+          <div className={styles["logo-container"]}>
             <Image
               src="/img/logosemletra.svg"
               alt="logo easycart"
               width={50}
               height={30}
+              className={styles.logoIcon}
             />
             <Image
               src="/img/logocomnome.svg"
               alt="logo nome easycart"
-              width={100}
+              width={150}
               height={30}
+              className={styles.logoText}
             />
           </div>
+
           {/* barra de pesquisa */}
-          <div
-            className="form-control flex justify-center"
-            style={{ marginLeft: "-350px" }}
-          >
+          <div className={`${styles["search-form"]} form-control`}>
             <form className="flex items-center relative">
               <input
                 type="text"
                 placeholder="Buscar produtos, marcas e muito mais…"
-                className={`input input-bordered nav-search-input w-full ${inputBgColor} ${textGrayColor}`}
-                style={{
-                  minWidth: "588px",
-                  maxWidth: "600px",
-                  height: "40px",
-                  padding: "10px 15px",
-                  borderRadius: "2px",
-                  boxShadow: "0 1px 2px 0 rgba(0, 0, 0, .2)",
-                  borderColor: "rgba(0, 0, 0, 0)",
-                  fontSize: "16px",
-                  fontFamily: "inherit",
-                  color: "rgba(0, 0, 0, .898)",
-                }}
+                className={`${styles["search-input"]} input input-bordered nav-search-input`}
                 id="cb1-edit"
                 name="as_word"
                 maxLength="120"
@@ -69,22 +47,9 @@ const Header = () => {
                 aria-expanded="false"
                 role="combobox"
               />
-              <span
-                className="absolute right-12 top-1/2 transform -translate-y-1/2 border-l border-gray-400"
-                style={{ height: "50%" }}
-              ></span>
-              <button
-                type="submit"
-                className="absolute right-0 top-0 bottom-0 flex justify-center items-center"
-                style={{
-                  width: "47px",
-                  height: "100%",
-                  backgroundColor: "white",
-                  borderRadius: "0 2px 2px 0",
-                }}
-              >
+              <button type="submit" className={styles["search-button"]}>
                 <Image
-                  src="/img/find.svg" // Substitua pelo caminho correto da sua imagem
+                  src="/img/find.svg"
                   alt="Buscar"
                   width={20}
                   height={20}
@@ -93,41 +58,30 @@ const Header = () => {
             </form>
           </div>
           {/* botão mudar tema */}
-          <div>
+          <div className="tema flex items-center space-x-4">
             <ToggleThemeButton />
-          </div>
-
-          <div className="flex-none space-x-4 flex items-center">
-            <label
-              className="flex items-center cursor-pointer gap-2"
-              style={{ marginLeft: "-350px" }}
-            ></label>
-            <div
-              className="dropdown dropdown-end"
-              style={{ marginLeft: "20px" }}
-            >
+            {/* carrinho de compra */}
+            <div className="dropdown dropdown-end">
               <div
                 tabIndex={0}
                 role="button"
-                className={`btn btn-ghost btn-circle ${hoverBgColor}`}
+                className={`btn btn-ghost btn-circle hover:bg-gray-400`}
               >
                 <div className="indicator">
                   <Image
-                    src="/img/cart-icon.png" // Substitua pelo caminho correto da sua imagem
+                    src="/img/cartbuton.svg"
                     alt="Carrinho"
-                    width={20}
+                    width={35}
                     height={20}
                   />
-                  <span
-                    className={`badge badge-sm indicator-item ${indicatorBgColor} ${textColor}`}
-                  >
+                  <span className="badge badge-sm indicator-item bg-red-600 text-primary">
                     8
                   </span>
                 </div>
               </div>
               <div
                 tabIndex={0}
-                className={`card card-compact dropdown-content ${menuBgColor} ${textGrayColor} z-[1] mt-3 w-52 shadow`}
+                className={`card card-compact dropdown-content bg-white text-primary z-[1] mt-3 w-52 shadow`}
               >
                 <div className="card-body">
                   <span className="text-lg font-bold">8 Items</span>
@@ -140,17 +94,16 @@ const Header = () => {
                 </div>
               </div>
             </div>
-            <div
-              className="dropdown dropdown-end"
-              style={{ marginLeft: "20px" }}
-            >
+
+            {/* botao user */}
+            <div className="dropdown dropdown-end2">
               <div
                 tabIndex={0}
                 role="button"
-                className={`btn btn-ghost btn-circle avatar ${hoverBgColor}`}
+                className={`btn btn-ghost btn-circle avatar hover:bg-gray-400`}
               >
                 <Image
-                  src="/img/user.svg" // Substitua pelo caminho correto da sua imagem
+                  src="/img/user.svg"
                   alt="Usuário"
                   width={38}
                   height={38}
@@ -158,30 +111,29 @@ const Header = () => {
               </div>
               <ul
                 tabIndex={0}
-                className={`menu menu-sm dropdown-content ${menuBgColor} ${textGrayColor} rounded-box z-[1] mt-3 w-52 p-2 shadow`}
+                className={`menu menu-sm dropdown-content bg-white text-primary rounded-box z-[1] mt-3 w-52 p-2 shadow`}
               >
                 <li>
                   <Link href="/admin" passHref>
                     Meus Produtos
-                    <span className={`badge ${badgeBgColor} ${textColor}`}>
-                      New
-                    </span>
+                    <span className="badge bg-green-600 text-primary">New</span>
                   </Link>
                 </li>
                 <li>
-                  <a className={menuHoverBgColor}>Settings</a>
+                  <a className="hover:bg-gray-200">Settings</a>
                 </li>
                 <li>
-                  <a className={menuHoverBgColor}>Logout</a>
+                  <a className="hover:bg-gray-200">Logout</a>
                 </li>
               </ul>
             </div>
           </div>
         </div>
       </div>
+      {/* linha horizontal embaixo do header */}
       <div
         className="w-full h-[2px]"
-        style={{ backgroundColor: primaryColor }}
+        style={{ backgroundColor: "var(--primary-color)" }}
       ></div>
     </>
   );
