61aa65b7802445adeedb87109943b7ec123928b2
Marcos Paulo <marcos.paulo.s.m.filho@gmail.com>
2024-09-01 21:59:33 -0300

    mudando header para tail wind

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,8 @@ import React, { useState } from 'react';
 import Link from "next/link";
 import Image from "next/image";
 import ToggleThemeButton from "./ToggleThemeButton";
-import styles from "../styles/Header.module.css"; // Importando o CSS corretamente
 import ShoppingCart from "./ShoppingCart";
-import {useCart} from '../contexts/CartContext';
+import { useCart } from '../contexts/CartContext';
 
 const Header = () => {
   const [isCartOpen, setIsCartOpen] = useState(false);
@@ -13,144 +12,138 @@ const Header = () => {
 
   return (
     <>
-      <div className={styles.navbar}>
-        <div className={`${styles["flex-container"]} flex-1`}>
+      <div className="px-4 py-4 sm:px-4 flex justify-between items-center">
+        <div className="flex items-center gap-2">
           {/* logo */}
-          <div className={styles["logo-container"]}>
-            <Image
-              src="/img/logosemletra.svg"
-              alt="logo easycart"
-              width={50}
-              height={30}
-              className={styles.logoIcon}
-            />
-            <Image
-              src="/img/logocomnome.svg"
-              alt="logo nome easycart"
-              width={150}
-              height={30}
-              className={styles.logoText}
-            />
-          </div>
+          <Image
+            src="/img/logosemletra.svg"
+            alt="logo easycart"
+            width={50}
+            height={30}
+          />
+          <Image
+            src="/img/logocomnome.svg"
+            alt="logo nome easycart"
+            width={150}
+            height={30}
+          />
+        </div>
 
-          {/* barra de pesquisa */}
-          <div className={`${styles["search-form"]} form-control`}>
-            <form className="flex items-center relative">
-              <input
-                type="text"
-                placeholder="Buscar produtos, marcas e muito mais…"
-                className={`${styles["search-input"]} input input-bordered nav-search-input`}
-                id="cb1-edit"
-                name="as_word"
-                maxLength="120"
-                autoCapitalize="off"
-                autoCorrect="off"
-                spellCheck="false"
-                autoComplete="off"
-                aria-activedescendant=""
-                aria-controls="cb1-list"
-                aria-autocomplete="list"
-                aria-expanded="false"
-                role="combobox"
+        {/* barra de pesquisa */}
+        <div className="flex items-center relative">
+          <form className="flex items-center relative">
+            <input
+              type="text"
+              placeholder="Buscar produtos, marcas e muito mais…"
+              className="min-w-[58px] w-[588px] h-10 p-2.5 rounded-l border-none shadow-md text-base bg-white text-black"
+              id="cb1-edit"
+              name="as_word"
+              maxLength="120"
+              autoCapitalize="off"
+              autoCorrect="off"
+              spellCheck="false"
+              autoComplete="off"
+              aria-activedescendant=""
+              aria-controls="cb1-list"
+              aria-autocomplete="list"
+              aria-expanded="false"
+              role="combobox"
+            />
+            <button type="submit" className="w-12 h-10 bg-white rounded-r flex justify-center items-center shadow-md">
+              <Image
+                src="/img/find.svg"
+                alt="Buscar"
+                width={20}
+                height={20}
               />
-              <button type="submit" className={styles["search-button"]}>
+            </button>
+          </form>
+        </div>
+
+        {/* botão mudar tema, carrinho de compra, e usuário */}
+        <div className="flex items-center space-x-4">
+          <ToggleThemeButton />
+          {/* carrinho de compra */}
+          <div className="dropdown dropdown-end">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle hover:bg-gray-400"
+            >
+              <div className="indicator">
                 <Image
-                  src="/img/find.svg"
-                  alt="Buscar"
-                  width={20}
+                  src="/img/cartbuton.svg"
+                  alt="Carrinho"
+                  width={35}
                   height={20}
                 />
-              </button>
-            </form>
-          </div>
-          {/* botão mudar tema */}
-          <div className="tema flex items-center space-x-4">
-            <ToggleThemeButton />
-            {/* carrinho de compra */}
-            <div className="dropdown dropdown-end">
-              <div
-                tabIndex={0}
-                role="button"
-                className={`btn btn-ghost btn-circle hover:bg-gray-400`}
-              >
-                <div className="indicator">
-                  <Image
-                    src="/img/cartbuton.svg"
-                    alt="Carrinho"
-                    width={35}
-                    height={20}
-                  />
-                  <span className="badge badge-sm indicator-item bg-red-600 text-primary">
+                <span className="badge badge-sm indicator-item bg-red-600 text-primary">
                   {cartItems.length}
-                  </span>
-                </div>
+                </span>
               </div>
-              <div
-                tabIndex={0}
-                className={`card card-compact dropdown-content bg-white text-primary z-[1] mt-3 w-52 shadow`}
-              >
-                <div className="card-body">
-                  <span className="text-lg font-bold">
-                    {cartItems.length} Produto(s)
-                  </span>
-                  <span className="text-info">
-                    <p>Subtotal:
+            </div>
+            <div
+              tabIndex={0}
+              className="card card-compact dropdown-content bg-white text-primary z-[1] mt-3 w-52 shadow"
+            >
+              <div className="card-body">
+                <span className="text-lg font-bold">
+                  {cartItems.length} Produto(s)
+                </span>
+                <span className="text-info">
+                  <p>Subtotal:
                     R$
                     {cartItems.reduce((total, item) => {
-                    const priceNumber = parseFloat(item.price.replace('R$', '').replace(',', '.').trim());
-                    return total + priceNumber * item.quantity;
+                      const priceNumber = parseFloat(item.price.replace('R$', '').replace(',', '.').trim());
+                      return total + priceNumber * item.quantity;
                     }, 0).toFixed(2)}
-                </p>
-                  </span>
-                  <div className="card-actions">
+                  </p>
+                </span>
+                 <div className="card-actions">
                   <button onClick={toggleCart}>
                     Ver Carrinho
                   </button>
-                  </div>
                 </div>
               </div>
             </div>
+          </div>
 
-            {/* botao user */}
-            <div className="dropdown dropdown-end2">
-              <div
-                tabIndex={0}
-                role="button"
-                className={`btn btn-ghost btn-circle avatar hover:bg-gray-400`}
-              >
-                <Image
-                  src="/img/user.svg"
-                  alt="Usuário"
-                  width={38}
-                  height={38}
-                />
-              </div>
-              <ul
-                tabIndex={0}
-                className={`menu menu-sm dropdown-content bg-white text-primary rounded-box z-[1] mt-3 w-52 p-2 shadow`}
-              >
-                <li>
-                  <Link href="/admin" passHref>
-                    Meus Produtos
-                    <span className="badge bg-green-600 text-primary">New</span>
-                  </Link>
-                </li>
-                <li>
-                  <a className="hover:bg-gray-200">Settings</a>
-                </li>
-                <li>
-                  <a className="hover:bg-gray-200">Logout</a>
-                </li>
-              </ul>
+          {/* botao user */}
+          <div className="dropdown dropdown-end">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle avatar hover:bg-gray-400"
+            >
+              <Image
+                src="/img/user.svg"
+                alt="Usuário"
+                width={38}
+                height={38}
+              />
             </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-white text-primary rounded-box z-[1] mt-3 w-52 p-2 shadow"
+            >
+              <li>
+                <Link href="/admin" passHref>
+                  Meus Produtos
+                  <span className="badge bg-green-600 text-primary">New</span>
+                </Link>
+              </li>
+              <li>
+                <a className="hover:bg-gray-200">Settings</a>
+              </li>
+              <li>
+                <a className="hover:bg-gray-200">Logout</a>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
       {/* linha horizontal embaixo do header */}
-      <div
-        className="w-full h-[2px]"
-        style={{ backgroundColor: "var(--primary-color)" }}
-      ></div>
+      <div className="w-full h-[2px] bg-primary"></div>
       <ShoppingCart isOpen={isCartOpen} onClose={toggleCart} />
     </>
   );
