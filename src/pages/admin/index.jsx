cb6f1e30dfc9546b736e4efeb853f37fac3c9985
Matheus da Silva <contagodzillajapan@gmail.com>
2024-09-02 20:51:45 -0300

    Fix: Retrabalhando a organização do display

--- src/pages/admin/index.jsx
+++ src/pages/admin/index.jsx
@@ -6,9 +6,11 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { exampleProducts } from "../../../produtos/exemplos";
 import ProductRegisterModal from "@/components/ProductRegisterModal";
+import SortBy from "@/components/SortBy";
 
 const Home = () => {
   const [products, setProducts] = useState(exampleProducts);
+  const [originalProducts, setOriginalProducts] = useState(exampleProducts);
   const [filteredProducts, setFilteredProducts] = useState(exampleProducts);
   const [isRegisterModalOpen, setIsRegisterModalOpen] = useState(false);
 
@@ -17,6 +19,7 @@ const Home = () => {
       try {
         const response = await api.get("/products");
         setProducts(response.data);
+        setOriginalProducts(response.data);
         setFilteredProducts(response.data);
       } catch (error) {
         console.error("Failed to fetch products:", error);
@@ -48,6 +51,34 @@ const Home = () => {
     handleCloseRegisterModal();
   };
 
+  const handleSortChange = (sortOption) => {
+    let sortedProducts = [...filteredProducts];
+    switch (sortOption) {
+      case "price-asc":
+        sortedProducts.sort((a, b) => parseFloat(a.price.replace('R$ ', '').replace(',', '.')) - parseFloat(b.price.replace('R$ ', '').replace(',', '.')));
+        break;
+      case "price-desc":
+        sortedProducts.sort((a, b) => parseFloat(b.price.replace('R$ ', '').replace(',', '.')) - parseFloat(a.price.replace('R$ ', '').replace(',', '.')));
+        break;
+      case "name-asc":
+        sortedProducts.sort((a, b) =>
+          a.name.localeCompare(b.name)
+        );
+        break;
+      case "name-desc":
+        sortedProducts.sort((a, b) =>
+          b.name.localeCompare(a.name)
+        );
+        break;
+        case "":
+          sortedProducts = [...originalProducts];
+          break;
+      default:
+        sortedProducts = [...filteredProducts];
+    }
+    setFilteredProducts(sortedProducts);
+  };
+
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
@@ -55,17 +86,19 @@ const Home = () => {
         <div className="flex">
           <FilterSidebar onFilterChange={handleFilterChange} />
           <div className="p-4 flex-grow">
+              <h2 className="titulo">Gerenciar Produtos</h2>
+              <div className="flex justify-evenly items-center mb-4">
+                <button 
+                  type="button" 
+                  className="register" 
+                  onClick={handleOpenRegisterModal}
+                >
+                  Cadastrar Produto
+                </button>
+                <SortBy onSortChange={handleSortChange} />
+              </div>
+              
             <div className="productContainerAdmin">
-            <button 
-                type="button" 
-                className="register" 
-                onClick={handleOpenRegisterModal}
-              >
-                Cadastrar Produto
-              </button>
-              {filteredProducts.map((product) => (
-                <ProductCardAdmin key={product.id} product={product} />
-              ))}
               {filteredProducts.map((product) => (
                 <ProductCardAdmin key={product.id} product={product} />
               ))}
