04686554727e7da220ce025f518bc8025f56bee9
RodrigoFontes377 <rodrigofontes377@gmail.com>
2024-09-05 15:29:35 -0300

    Fix: convertendo css para Tailwind

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,7 +1,6 @@
-import React, { useState } from 'react';
-import Image from 'next/image';
-import ProductModal from './ProductModal';
-import styles from '../styles/ProductCard.module.css';
+import React, { useState } from "react";
+import Image from "next/image";
+import ProductModal from "./ProductModal";
 
 const ProductCard = ({ product }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -11,45 +10,51 @@ const ProductCard = ({ product }) => {
 
   return (
     <>
-      <div className={styles.productCard} onClick={openModal}>
-        <div className={styles.imageWrapper}>
-          <Image className={styles.productImage}
-            src={product.image} 
-            alt={product.name} 
-            width={100} 
-            height={100} 
+      <div
+        className="relative flex flex-col justify-center items-center border border-gray-300 rounded-lg p-2 w-64 h-96 shadow-lg text-center transition-transform duration-200 ease-in-out hover:scale-105 group"
+        onClick={openModal}
+      >
+        <div className="flex justify-center items-center h-40 w-full overflow-hidden">
+          <Image
+            className="w-auto h-full object-contain"
+            src={product.image}
+            alt={product.name}
+            width={100}
+            height={100}
             layout="responsive"
           />
-          <div className={styles.cartIcon}>
-            <Image 
-              src="/icons/cart.svg" 
-              alt="Adicionar ao Carrinho" 
-              width={30} 
-              height={30} 
-            />
-          </div>
-          <div className={styles.detailsIcon}>
-            <Image 
-              src="/icons/product-details.svg" 
-              alt="Detalhes do Produto" 
-              width={30} 
-              height={30} 
-            />
-          </div>
         </div>
-        <div className={styles.productInfo}>
-          <h2 className={styles.productName}>{product.name}</h2>
-          <p className={styles.productPrice}>{product.price}</p>
-          <p className={styles.productStatus}>{product.status}</p>
+
+        {/* Ícones que aparecem ao passar o mouse */}
+        <div className="absolute bottom-2 right-2 opacity-0 m-2 group-hover:opacity-100 transition-opacity duration-200 ease-in-out">
+          <Image
+            src="/icons/cart.svg"
+            alt="Adicionar ao Carrinho"
+            width={30}
+            height={30}
+          />
+        </div>
+        <div className="absolute bottom-2 left-2 opacity-0 m-2 group-hover:opacity-100 transition-opacity duration-200 ease-in-out">
+          <Image
+            src="/icons/product-details.svg"
+            alt="Detalhes do Produto"
+            width={30}
+            height={30}
+          />
+        </div>
+
+        <div className="mt-4 flex flex-col items-center">
+          <h2 className="text-blue-600 text-lg font-bold mb-1">
+            {product.name}
+          </h2>
+          <p className="text-blue-800 text-xl font-bold mb-2">
+            {product.price}
+          </p>
+          <p className="text-green-600 text-sm font-bold">{product.status}</p>
         </div>
       </div>
 
-      {isModalOpen && (
-        <ProductModal 
-          product={product} 
-          onClose={closeModal} 
-        />
-      )}
+      {isModalOpen && <ProductModal product={product} onClose={closeModal} />}
     </>
   );
 };
